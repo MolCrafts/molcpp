c7e1985beb9ff04246e9554417705ca30ff4c9cc
Roy-Kid <lijichen365@126.com>
2023-04-04 10:32:21 +0800

    add test of property and property_map

--- include/property.h
+++ include/property.h
@@ -1,98 +1,129 @@
 #include <map>
 #include <variant>
 
-template <typename... Ts>
-class Property
+namespace molcpp
 {
-public:
-    using variant_type = std::variant<Ts...>;
 
-    Property() : value_(variant_type{}) {}
-
-    template <typename T>
-    Property(const T &value) : value_(value) {}
-
-    template <typename T>
-    Property<Ts...> &operator=(const T &value)
+    template <typename... Ts>
+    class Property
     {
+    public:
+        using variant_type = std::variant<Ts...>;
 
-        value_ = value;
-        return *this;
-    }
+        Property() : value_(variant_type{}) {}
 
-    template <typename T>
-    T &get()
-    {
-        return std::get<T>(value_);
-    }
+        template <typename T>
+        Property(const T &value) : value_(value) {}
 
-    template <typename T>
-    const T &get() const
-    {
-        return std::get<T>(value_);
-    }
-
-    template <typename Visitor>
-    decltype(auto) visit(Visitor &&visitor) const
-    {
-        return std::visit(std::forward<Visitor>(visitor), value_);
-    }
-
-    template <typename Visitor>
-    decltype(auto) visit(Visitor &&visitor)
-    {
-        return std::visit(std::forward<Visitor>(visitor), value_);
-    }
-
-private:
-    variant_type value_;
-};
-
-template <typename... Ts>
-class Property_map : public std::map<std::string, Property<Ts...>>
-{
-public:
-    using base_type = std::map<std::string, Property<Ts...>>;
-
-    using typename base_type::key_type;
-    using typename base_type::mapped_type;
-    using typename base_type::value_type;
-
-    using base_type::base_type;
-
-    template <typename T>
-    void set(const key_type &key, const T &value)
-    {
-        auto it = this->find(key);
-        if (it == this->end())
+        template <typename T>
+        Property<Ts...> &operator=(const T &value)
         {
-            this->emplace(key, value);
-        }
-        else
-        {
-            it->second = value;
-        }
-    }
 
-    template <typename T>
-    T &get(const key_type &key)
-    {
-        auto it = this->find(key);
-        if (it == this->end())
-        {
-            throw std::out_of_range("Key not found");
+            value_ = value;
+            return *this;
         }
-        return it->second.template get<T>();
-    }
 
-    template <typename T>
-    const T &get(const key_type &key) const
-    {
-        auto it = this->find(key);
-        if (it == this->end())
+        template <typename T>
+        bool operator==(const T &value) const
         {
-            throw std::out_of_range("Key not found");
+            // return std::visit([&](auto &&arg) { return arg == value; }, value_);
+            return value_ == value;
         }
-        return it->second.template get<T>();
-    }
-};
+
+        template <typename... Us>
+        bool operator==(const Property<Us...>& other) const
+        {
+            return value_ == other.value_;
+        }
+
+        template <typename T>
+        T &get()
+        {
+            return std::get<T>(value_);
+        }
+
+        template <typename T>
+        const T &get() const
+        {
+            return std::get<T>(value_);
+        }
+
+        template <typename T>
+        bool is() const
+        {
+            return std::holds_alternative<T>(value_);
+        }
+
+    private:
+        variant_type value_;
+    };
+
+    template <typename... Ts>
+    class Property_map
+    {
+    public:
+        using key_type = std::string;
+        using value_type = Property<Ts...>;
+        using container_type = std::map<key_type, value_type>;
+
+    private:
+        container_type m_map;
+
+    public:
+        template <typename T>
+        void set(const key_type &key, const T &value)
+        {
+            auto it = m_map.find(key);
+            if (it == m_map.end())
+            {
+                m_map.emplace(key, value);
+            }
+            else
+            {
+                it->second = value;
+            }
+        }
+
+        template <typename T>
+        T &get(const key_type &key)
+        {
+            auto it = m_map.find(key);
+            if (it == m_map.end())
+            {
+                throw std::out_of_range("Key not found");
+            }
+            return it->second.template get<T>();
+        }
+
+        template <typename T>
+        const T &get(const key_type &key) const
+        {
+            auto it = m_map.find(key);
+            if (it == m_map.end())
+            {
+                throw std::out_of_range("Key not found");
+            }
+            return it->second.template get<T>();
+        }
+        std::vector<key_type> key() const
+        {
+            std::vector<key_type> keys;
+            for (const auto &pair : m_map)
+            {
+                keys.push_back(pair.first);
+            }
+            return keys;
+        }
+
+        std::vector<value_type> value() const
+        {
+            std::vector<value_type> values;
+            for (const auto &pair : m_map)
+            {
+                values.push_back(pair.second);
+            }
+            return values;
+        }
+
+    };
+}
